ff9a62eae5607fd59f43e77550c46f01ed3fac42
BluPearl-J <ogheneokeno@gmail.com>
2025-07-16 15:45:01 +0100

    Create numberfuel.py

---
+++ numberfuel.py
@@ -0,0 +1,12 @@
+PRICE_PER_LITER = 855 ;
+
+total_Budget = int( input ("Enter total budget"));
+
+print ("your total budget is", total_Budget);
+
+
+PRICE_PER_LITER = 855;
+
+amount_fuel_litres = total_Budget / PRICE_PER_LITER;
+
+print ("your AMOUNT FUELIN LITERS", amount_fuel_litres);
