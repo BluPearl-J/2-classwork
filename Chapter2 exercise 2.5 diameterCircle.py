8a87bcda6ffb08c11d2fa19553e35f115c7fc30c
BluPearl-J <ogheneokeno@gmail.com>
2025-07-19 10:55:43 +0100

    Create Chapter2 exercise 2.5 diameterCircle.py

---
+++ Chapter2 exercise 2.5 diameterCircle.py
@@ -0,0 +1,16 @@
+PI = 3.14159 ;
+
+radius = 2;
+
+diameter = 2 * radius ;
+
+print ("The diameter of the circle with radius 2 is",diameter );
+
+circumference = 2 * PI * radius;
+
+print ("The circumference of the circle with radius 2 is",circumference);
+
+area = PI * radius * radius;
+
+
+print ("The area of the circle with radius 2 is",area);
