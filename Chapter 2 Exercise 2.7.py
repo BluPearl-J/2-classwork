010003185291f98965a3e737dc3526fc6056c5d9
BluPearl-J <ogheneokeno@gmail.com>
2025-07-19 14:06:52 +0100

    Create Chapter 2 Exercise 2.7.py

---
+++ Chapter 2 Exercise 2.7.py
@@ -0,0 +1,31 @@
+# 2.7 (Multiples) Use if statements to determine whether 1024 is a multiple of 4 and
+# whether 2 is a multiple of 10. (Hint: Use the remainder operator.)
+
+
+
+
+NUMBER = 1024;
+
+TWO = 2;
+
+
+print ("your first number  is", NUMBER );
+print ("DETERMINING IF THE NUMBER IS  MULTIPLE OF FOUR .....: ", NUMBER);
+
+if (NUMBER % 4) == 0: 
+ print("THE NUMBER 1024 IS MULTIPLE OF 4 because it gives remainder 0 when divided by 4")
+
+else: 
+ print(" THE NUMBER 1024 IS NOT A MULTIPLE OF FOUR"); 
+
+
+
+print ("your second number  is", TWO);
+ 
+
+if (TWO% 10 ) == 0: 
+ print("THE NUMBER 2 IS MULTIPLE OF 10")
+
+else: 
+ print(" THE NUMBER 2 IS NOT A MULTIPLE OF TEN"); 
+
