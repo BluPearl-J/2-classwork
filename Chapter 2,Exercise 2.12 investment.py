8e23014706a4a4db1b2f52d16e217e15551aad61
BluPearl-J <ogheneokeno@gmail.com>
2025-07-19 12:54:52 +0100

    Create Chapter 2,Exercise 2.12 investment.py

---
+++ Chapter 2,Exercise 2.12 investment.py
@@ -0,0 +1,34 @@
+PRINCIPAL = 1000  
+
+RATE = 0.07;
+
+
+
+
+b = (1 + RATE);
+c= b**10;
+d= b**20;
+e= b**30;
+
+
+# a = PRINCIPAL * (1 + RATE)** n 
+
+
+print  ("your PRINCIPAL IS", PRINCIPAL );
+
+
+print  ("The formula for nth year investment return is PRINCIPAL * (1 + RATE)raised to power n "  );
+
+
+print  ("YOUR INVESTMENT RETURN AFTER 10 YEARS FOR 7% RATE IS", PRINCIPAL*c );
+
+
+print  ("YOUR INVESTMENT RETURN AFTER 20 YEARS FOR 7% RATE IS", PRINCIPAL*d );
+
+print  ("YOUR INVESTMENT RETURN AFTER 30 YEARS FOR 7% RATE IS", PRINCIPAL*e );
+
+
+
+
+
+
