24f21232a31220c7e218ab41f66362cb675565d8
BluPearl-J <ogheneokeno@gmail.com>
2025-07-22 15:04:50 +0100

    Add files via upload

---
+++ squareCube.py
@@ -0,0 +1,19 @@
+count=0
+while count < 50:
+  count = count + 1
+  print (count, end=" ")
+
+if count % 2==0:
+
+    print(count*count, end=" ")
+else:
+    print(count*count*count, end=" ")
+
+
+for number in range (2,50, 2):
+ print (number*number, end=" ")
+
+for number in range (3,50,3):
+
+ print (number*number*number, end=" ")
+ 
