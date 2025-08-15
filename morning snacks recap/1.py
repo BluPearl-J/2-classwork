71567e196e1c151cb27906198354601feb6b8795
BluPearl-J <ogheneokeno@gmail.com>
2025-08-15 09:36:51 +0100

    Add files via upload

---
+++ morning snacks recap/1.py
@@ -0,0 +1,7 @@
+#1 GIVEN A LIST OF NUMBERS, 
+#WRITE A PYTHON FUNCTION USING MAP() THAT SQAUERES EACH NUMBER IN THE LIST.
+Numbers=[1,2,3,4,5]
+
+
+mappedNumbers = [element ** 2 for element in range(1, 6)]
+print(mappedNumbers)
