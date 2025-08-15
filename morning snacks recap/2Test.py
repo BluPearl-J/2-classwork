c6ad5f7740b1a9dfb0320c517371c2c251bd2698
BluPearl-J <ogheneokeno@gmail.com>
2025-08-15 11:07:53 +0100

    Create 2Test.py

---
+++ morning snacks recap/2Test.py
@@ -0,0 +1,10 @@
+import unittest
+import cube
+
+class TestGetLengthFunction(unittest.TestCase): 
+        def test_that_get_length_array_function_exists(self)
+                     2.get_length_array(["dog","elephant"])
+
+        def test_that_get_length_array_function_return_correct_result ()
+             result=cube.get_cube([3,8])
+             self.assertEqual(result,[3,8])
