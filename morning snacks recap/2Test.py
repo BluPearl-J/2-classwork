8420331a11cd496a83ec8864c297f400d8a561d2
BluPearl-J <ogheneokeno@gmail.com>
2025-08-15 11:10:22 +0100

    Update 2Test.py

--- morning snacks recap/2Test.py
+++ morning snacks recap/2Test.py
@@ -6,5 +6,5 @@ class TestGetLengthFunction(unittest.TestCase):
                      2.get_length_array(["dog","elephant"])
 
         def test_that_get_length_array_function_return_correct_result ()
-             result=cube.get_cube([3,8])
+             result=2.get_length_array([3,8])
              self.assertEqual(result,[3,8])
