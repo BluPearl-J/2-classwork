af257239ffb4c02270ad7ed9c28f3b972069fba1
BluPearl-J <ogheneokeno@gmail.com>
2025-08-18 07:30:27 +0100

    Add files via upload

---
+++ snacks Code Leveling weekend snacks august 17th/Exercise13of21.py
@@ -0,0 +1,4 @@
+#Use map() to square all numbers from 1 to 10
+#map must have at least two arguments
+squares = list(map(lambda element: element**2, range(1, 10)))
+print(squares)
---
+++ snacks Code Leveling weekend snacks august 17th/maptest.py
@@ -0,0 +1,35 @@
+#Using reduce() and map(), find the product 
+#of the squares of numbers [1, 2, 3, 4].
+
+SOLVE2 2 2 COMPLETE TESTSS
+
+
+product=1;
+
+list(map(lambda element: element*product, reduce(lambda x: x **2 , numbers)))
+#pg xx textbook
+#In [9]: list(map(lambda x: x ** 2,filter(lambda x: x % 2 != 0, numbers)))
+
+#Using map(), convert all strings in 
+#['python', 'java', 'c++'] to uppercase.
+
+
+# list(map(lambda x: x.touppercase
+
+
+#2. Use map() to square all numbers from 1 to 10.
+
+# list(map(lambda x: x.touppercase
+
+#3. Given names = ['john', 'mary', 
+#'steve'], use map() to capitalize the first 
+#letter of each name.
+
+
+#4. Given prices = [100, 200, 300], use 
+#map() to add 10% tax to each price
+
+# list(map(lambda x: x+10%
+
+
+
