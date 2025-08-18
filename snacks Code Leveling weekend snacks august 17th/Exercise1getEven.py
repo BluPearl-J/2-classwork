aee16295776be419156672a5f39c4beab81fe3d8
BluPearl-J <ogheneokeno@gmail.com>
2025-08-18 07:53:54 +0100

    Update Exercise1getEven.py

--- snacks Code Leveling weekend snacks august 17th/Exercise1getEven.py
+++ snacks Code Leveling weekend snacks august 17th/Exercise1getEven.py
@@ -5,15 +5,7 @@
 result=list(filter(lambda x: x%2==0,range(1,21)))
 print(result)
 
-#3. Given names = ['john', 'mary', 
-#'steve'], use map() to capitalize the first 
-#letter of each name.
 
-
-#4. Given prices = [100, 200, 300], use 
-#map() to add 10% tax to each price
-
-# list(map(lambda x: x+10%
 
 
 
